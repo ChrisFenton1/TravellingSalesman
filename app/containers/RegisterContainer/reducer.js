3b2f6e5b6a614b607519ed4b9987ca60f0f6cc3c
mikeminhle <mike.minhle@allocatesoftware.com>
2019-11-29 16:41:57 +1100

    update logic to add to state with immutable js

--- app/containers/RegisterContainer/reducer.js
+++ app/containers/RegisterContainer/reducer.js
@@ -16,15 +16,7 @@ const initialState = fromJS({
 function registerContainerReducer(state = initialState, action) {
   switch (action.type) {
     case REGISTER:
-      return [...state, { ...action.user }];
-      // return {
-      //   ...state,
-      //   users: [...state.users, action.user],
-      // };
-
-      // const users = state.users ? state.users : [];
-      // users.push(action.user);
-      // return state.set('users', users);
+      return state.set(state.size, action.user);
     default:
       return state;
   }
